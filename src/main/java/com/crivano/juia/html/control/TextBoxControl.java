7bd30054e55f17738018a46347da392439c44d46
Renato Crivano <renato.crivano@gmail.com>
2023-02-23 17:42:37 -0300

    TextArea Auto Grow

--- src/main/java/com/crivano/juia/html/control/TextBoxControl.java
+++ src/main/java/com/crivano/juia/html/control/TextBoxControl.java
@@ -13,7 +13,6 @@ import com.webfirmframework.wffweb.tag.html.attribute.Type;
 import com.webfirmframework.wffweb.tag.html.attribute.core.AbstractAttribute;
 import com.webfirmframework.wffweb.tag.html.attribute.global.ClassAttribute;
 import com.webfirmframework.wffweb.tag.html.attribute.global.Id;
-import com.webfirmframework.wffweb.tag.html.attribute.global.Title;
 import com.webfirmframework.wffweb.tag.html.attributewff.CustomAttribute;
 import com.webfirmframework.wffweb.tag.html.formsandinputs.Input;
 import com.webfirmframework.wffweb.tag.html.formsandinputs.Label;
@@ -25,26 +24,31 @@ public class TextBoxControl {
 	public static void render(Div parent, ClassAttribute col, final FieldText vi) {
 		Section section = new Section(parent, new ClassAttribute(col.getAttributeValue() + " form-group")) {
 			{
-				Label lbl = Utils.label(this, vi);
-				AbstractHtml input;
-				Edit edit = vi.fld.getAnnotation(Edit.class);
-				if (edit.kind() == EditKindEnum.TEXTAREA) {
-					input = new TextArea(lbl, new ClassAttribute("form-control"), new Id(vi.fld.getName()),
-							new CustomAttribute("ng-model", vi.name), new Name(vi.fld.getName()));
-				} else {
-					AbstractAttribute attrType = HtmlTemplateBuilder.getAttr("type", vi.attr, null, null);
-					Type type;
-					if (attrType != null)
-						type = new Type(attrType.getAttributeValue());
-					else
-						type = new Type("text");
-					input = new Input(lbl, type, new ClassAttribute("form-control"), new Id(vi.fld.getName()),
-							new CustomAttribute("ng-model", vi.name), new Name(vi.fld.getName()));
-				}
-				if (vi.fld.isAnnotationPresent(NotNull.class))
-					input.addAttributes(new CustomAttribute("ng-required", "true"));
+				try {
+					Label lbl = Utils.label(this, vi);
+					AbstractHtml input;
+					Edit edit = vi.fld.getAnnotation(Edit.class);
+					if (edit.kind() == EditKindEnum.TEXTAREA) {
+						input = new TextArea(lbl, new ClassAttribute("form-control"), new Id(vi.fld.getName()),
+								new CustomAttribute("text-area-auto-expand"), new CustomAttribute("ng-model", vi.name),
+								new Name(vi.fld.getName()));
+					} else {
+						AbstractAttribute attrType = HtmlTemplateBuilder.getAttr("type", vi.attr, null, null);
+						Type type;
+						if (attrType != null)
+							type = new Type(attrType.getAttributeValue());
+						else
+							type = new Type("text");
+						input = new Input(lbl, type, new ClassAttribute("form-control"), new Id(vi.fld.getName()),
+								new CustomAttribute("ng-model", vi.name), new Name(vi.fld.getName()));
+					}
+					if (vi.fld.isAnnotationPresent(NotNull.class))
+						input.addAttributes(new CustomAttribute("ng-required", "true"));
 
-				HtmlTemplateBuilder.addAttr(vi.attr, input);
+					HtmlTemplateBuilder.addAttr(vi.attr, input);
+				} catch (Exception e) {
+					throw e;
+				}
 			}
 		};
 		HtmlTemplateBuilder.addAttr(vi.attrContainer, section);
