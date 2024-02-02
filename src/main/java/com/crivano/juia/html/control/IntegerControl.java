c1e28a21299cb6cdf20bb04e0726ad54c88e143f
Renato Crivano <renato.crivano@gmail.com>
2024-02-02 19:41:04 -0300

    Fixing width of integer input

--- src/main/java/com/crivano/juia/html/control/IntegerControl.java
+++ src/main/java/com/crivano/juia/html/control/IntegerControl.java
@@ -9,7 +9,6 @@ import com.webfirmframework.wffweb.tag.html.attribute.Name;
 import com.webfirmframework.wffweb.tag.html.attribute.Type;
 import com.webfirmframework.wffweb.tag.html.attribute.global.ClassAttribute;
 import com.webfirmframework.wffweb.tag.html.attribute.global.Id;
-import com.webfirmframework.wffweb.tag.html.attribute.global.Title;
 import com.webfirmframework.wffweb.tag.html.attributewff.CustomAttribute;
 import com.webfirmframework.wffweb.tag.html.formsandinputs.Input;
 import com.webfirmframework.wffweb.tag.html.formsandinputs.Label;
@@ -17,27 +16,16 @@ import com.webfirmframework.wffweb.tag.html.html5.stylesandsemantics.Section;
 import com.webfirmframework.wffweb.tag.html.stylesandsemantics.Div;
 
 public class IntegerControl {
-	public static void render(Div parent, ClassAttribute col,
-			final FieldInteger vi) {
+	public static void render(Div parent, ClassAttribute col, final FieldInteger vi) {
 		Section section = new Section(parent, new ClassAttribute(col.getAttributeValue() + " form-group")) {
 			{
-				Utils.label(this, vi);
-
-				new Label(this, new CustomAttribute("for", vi.fld.getName()),
-						new Title(vi.hint), new ClassAttribute("input")) {
-					{
-						Input input = new Input(this, new Type("text"),
-								new ClassAttribute("form-control"), new Id(
-										vi.fld.getName()), new CustomAttribute(
-										"ng-model", vi.name), new Name(
-										vi.fld.getName()), new CustomAttribute(
-										"ui-number-mask", "0"));
-						if (vi.fld.isAnnotationPresent(NotNull.class))
-							input.addAttributes(new CustomAttribute(
-									"ng-required", "true"));
-						HtmlTemplateBuilder.addAttr(vi.getAttr(), input, "__FIELD_NAME__", vi.name);
-					}
-				};
+				Label lbl = Utils.label(this, vi);
+				Input input = new Input(lbl, new Type("text"), new ClassAttribute("form-control"),
+						new Id(vi.fld.getName()), new CustomAttribute("ng-model", vi.name), new Name(vi.fld.getName()),
+						new CustomAttribute("ui-number-mask", "0"));
+				if (vi.fld.isAnnotationPresent(NotNull.class))
+					input.addAttributes(new CustomAttribute("ng-required", "true"));
+				HtmlTemplateBuilder.addAttr(vi.getAttr(), input, "__FIELD_NAME__", vi.name);
 			}
 		};
 		HtmlTemplateBuilder.addAttr(vi.attrContainer, section, "__FIELD_NAME__", vi.name);
